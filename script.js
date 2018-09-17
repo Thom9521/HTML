0c7f03032779be8a58e447a45a99dc68077467eb
Thom9521 <Thom9521@edu.easj.dk>
2018-09-17 18:50:06 +0200

    HuskespilTilføjelser

---
+++ script.js
@@ -0,0 +1,78 @@
+function checkF1(){
+    if (document.getElementById("firefox1").src ="billeder/squareRed.jpg"){
+        document.getElementById("firefox1").src = "billeder/firefox.png";
+    }
+}
+    function checkF2(){ 
+     if(document.getElementById("firefox2").src = "billeder/squareRed.jpg"){
+        document.getElementById("firefox2").src = "billeder/firefox.png";
+    }}
+    function checkC1(){
+    if(document.getElementById("chrome1").src ="billeder/squareRed.jpg"){
+        document.getElementById("chrome1").src = "billeder/chrome.png"
+    }}
+function checkC2(){
+    if(document.getElementById("chrome2").src ="billeder/squareRed.jpg"){
+        document.getElementById("chrome2").src = "billeder/chrome.png"
+    }
+}
+function checkD1(){
+    if(document.getElementById("discord1").src ="billeder/squareRed.jpg"){
+        document.getElementById("discord1").src = "billeder/discord.png"
+    }
+}
+function checkD2(){
+    if(document.getElementById("discord2").src ="billeder/squareRed.jpg"){
+        document.getElementById("discord2").src = "billeder/discord.png"
+    }
+}
+function checkE1(){
+    if(document.getElementById("explorer1").src ="billeder/squareRed.jpg"){
+        document.getElementById("explorer1").src = "billeder/explorer.png"
+    }
+}
+function checkE2(){
+    if(document.getElementById("explorer2").src ="billeder/squareRed.jpg"){
+        document.getElementById("explorer2").src = "billeder/explorer.png"
+    }
+}
+function checkG1(){
+    if(document.getElementById("github1").src ="billeder/squareRed.jpg"){
+        document.getElementById("github1").src = "billeder/github.png"
+    }    
+}
+function checkG2(){
+    if(document.getElementById("github2").src ="billeder/squareRed.jpg"){
+        document.getElementById("github2").src = "billeder/github.png"
+    }    
+}
+function checkI1(){
+    if(document.getElementById("intellij1").src ="billeder/squareRed.jpg"){
+        document.getElementById("intellij1").src = "billeder/intellij.png"
+    }    
+}
+function checkI2(){
+    if(document.getElementById("intellij2").src ="billeder/squareRed.jpg"){
+        document.getElementById("intellij2").src = "billeder/intellij.png"
+    }    
+}
+function checkS1(){
+    if(document.getElementById("safari1").src ="billeder/squareRed.jpg"){
+        document.getElementById("safari1").src = "billeder/safari.png"
+    }    
+}
+function checkS2(){
+    if(document.getElementById("safari2").src ="billeder/squareRed.jpg"){
+        document.getElementById("safari2").src = "billeder/safari.png"
+    }    
+}
+function checkW1(){
+    if(document.getElementById("windows1").src ="billeder/squareRed.jpg"){
+        document.getElementById("windows1").src = "billeder/windows.png"
+    }    
+}
+function checkW2(){
+    if(document.getElementById("windows2").src ="billeder/squareRed.jpg"){
+        document.getElementById("windows2").src = "billeder/windows.png"
+    }    
+}
