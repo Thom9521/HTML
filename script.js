4996592beea5fde92d40851b89bf9d0686f89982
Thom9521 <Thom9521@edu.easj.dk>
2018-10-28 23:04:50 +0100

    Music and stuff

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+
+
 function github(){
     location.replace("https://github.com/Thom9521/Hjemmeside");
 }
-
+//Virker ikke helt optimalt eftersom lyden ikke burde spilles når man kan se spøgelset.
 function secretGhost() {
-        $(document).ready(function(){
+    if(document.getElementById("ghost").display = "none"){
+        var audio = document.getElementById("ghostAudio");
+        audio.play();
+    }
+    $(document).ready(function(){
                 $("#ghost").fadeIn(2000);
             });
         }
 
 function foundGhost(){
-    alert("Du er en ægte spøgelsesjæger!")
+    alert("Du er en ægte spøgelsesjæger!");
+    playHappyMusic();
+    document.getElementById("HalloweenAudio").pause();
+
+   
+}
+function playHappyMusic(){
+    var audio = document.getElementById("SLLAudio");
+    audio.play();
+
+}
+window.onload = function() {
+    document.getElementById("HalloweenAudio").play();
 }
