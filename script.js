b88f9d225082db3895654475af64a9718411c272
Thom9521 <Thom9521@edu.easj.dk>
2018-10-28 18:30:53 +0100

    Secret Ghost

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
 function github(){
     location.replace("https://github.com/Thom9521/Hjemmeside");
+}
+
+function secretGhost() {
+    var x = document.getElementById("ghost");
+    if (x.style.display === "block") {
+       x.style.display = "none";
+     
+    } else {
+        x.style.display = "block";
+    }
 }
