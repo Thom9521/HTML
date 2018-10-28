5312ec930f672cd6f3310c183fd1cc10209055bb
Thom9521 <Thom9521@edu.easj.dk>
2018-10-28 19:29:54 +0100

    Animation til spøgelse

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
 function github(){
     location.replace("https://github.com/Thom9521/Hjemmeside");
 }
 
 function secretGhost() {
-    var x = document.getElementById("ghost");
-    if (x.style.display === "block") {
-       x.style.display = "none";
-     
-    } else {
-        x.style.display = "block";
-    }
+        $(document).ready(function(){
+                $("#ghost").fadeIn(2000);
+            });
+        }
+
+function foundGhost(){
+    alert("Du en ægte spøgelsesjæger!")
 }
