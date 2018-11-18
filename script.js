d0c6b66c34c569e6aaa87dee27f5705681c62f34
Thom9521 <Thom9521@edu.easj.dk>
2018-11-18 22:33:29 +0100

    Lidt ekstra guf

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function secretGift() {
         //var audio = document.getElementById("ghostAudio");
         //audio.play();
     }
-    $(document).ready(function(){
+        $(document).ready(function(){
                 $("#gift").fadeIn(2000);
             });
+            playGiftAudio();
         }
 
 function foundGift(){
@@ -21,9 +22,29 @@ function foundGift(){
     $(document).ready(function(){
         $("#mickeyText").fadeIn(1500);
     });
-
+playSurpriseAudio();
 }
 
 function mickeyBandit(){
     alert("Den bandit!");
 }
+function playGiftAudio(){
+    var audioGift = document.getElementById("giftAudio");
+    audioGift.play();
+}
+function playSurpriseAudio(){
+    var audioSurprise = document.getElementById("surpriseAudio");
+    audioSurprise.play();
+}
+function playChristmasAudio(){
+    var audioChristmas = document.getElementById("christmasAudio");
+    audioChristmas.play();
+}
+function pauseChristmasAudio(){
+    var audioChristmas = document.getElementById("christmasAudio");
+    audioChristmas.pause();
+}
+function github(){
+    var win = window.open("https://github.com/Thom9521/Hjemmeside", "_blank");
+    win.focus;
+ }
