d0b8f30ff975582f4af228dcb2452fbc2cdfbb5e
Thom9521 <Thom9521@edu.easj.dk>
2019-02-07 18:59:06 +0100

    Tilføjet mad-siden og logo på alle sider

---
+++ mad/script.js
@@ -0,0 +1,67 @@
+function hentMad(){
+    var randomNumber = Math.floor(Math.random()*21+1);
+
+    if(randomNumber === 1){
+        document.getElementById("resultat").innerHTML = "Hjemmelavet Pizza"
+    }
+    else if(randomNumber === 2){
+        document.getElementById("resultat").innerHTML = "Tomatsuppe 🥣"
+    }
+    else if(randomNumber === 3){
+        document.getElementById("resultat").innerHTML = "Kylling i Karry"
+    }
+    else if(randomNumber === 4){
+        document.getElementById("resultat").innerHTML = "Boller i Karry"
+    }
+    else if(randomNumber === 5){
+        document.getElementById("resultat").innerHTML = "Chili Con Carne"
+    }
+    else if(randomNumber === 6){
+        document.getElementById("resultat").innerHTML = "Millionbøf med Pasta/Kartoffelmos"
+    }
+    else if(randomNumber === 7){
+        document.getElementById("resultat").innerHTML = "Mørbradgryde 🥘"
+    }
+    else if(randomNumber === 8){
+        document.getElementById("resultat").innerHTML = "Pasta med Kødsovs 🍝"
+    }
+    else if(randomNumber === 9){
+        document.getElementById("resultat").innerHTML = "Carbonara"
+    }
+    else if(randomNumber === 10){
+        document.getElementById("resultat").innerHTML = "Meksikansk 🌮"
+    }
+    else if(randomNumber === 11){
+        document.getElementById("resultat").innerHTML = "Lasagne"
+    }
+    else if(randomNumber === 12){
+        document.getElementById("resultat").innerHTML = "Risengrød 🍚"
+    }
+    else if(randomNumber === 13){
+        document.getElementById("resultat").innerHTML = "En Lækker Bøf m. Tilbehør 🥩"
+    }
+    else if(randomNumber === 14){
+        document.getElementById("resultat").innerHTML = "Frikadeller"
+    }
+    else if(randomNumber === 15){
+        document.getElementById("resultat").innerHTML = "Fiskefrikadeller"
+    }
+    else if(randomNumber === 16){
+        document.getElementById("resultat").innerHTML = "Hjemmelavet Burger 🍔"
+    }
+    else if(randomNumber === 17){
+        document.getElementById("resultat").innerHTML = "Brændende Kærlighed"
+    }
+    else if(randomNumber === 18){
+        document.getElementById("resultat").innerHTML = "Tarteletter"
+    }
+    else if(randomNumber === 19){
+        document.getElementById("resultat").innerHTML = "Hakkebøffer m. Tilbehør"
+    }
+    else if(randomNumber === 20){
+        document.getElementById("resultat").innerHTML = "Æggekage"
+    }
+    else if(randomNumber === 21){
+        document.getElementById("resultat").innerHTML = "Spise ude? 😸🥂"
+    }
+}
