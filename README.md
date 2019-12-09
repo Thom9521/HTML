fe8dcc5f6da7aa226698a347cdb3f809847bd9fd
Thom9521 <Thom9521@edu.easj.dk>
2019-12-09 17:58:42 +0100

    Follow up...

--- README.md
+++
@@ -1,6 +0,0 @@
-<h1> Hjemmeside </h1> 
-Dette repository indeholder data fra min hjemmeside.
-
-Opbygningen af hjemmesiden bliver opdateret løbende.
-
-Hjemmeside: <b>[Techthomas.dk](http://www.Techthomas.dk/)</b>
---
+++ testing/script.js
@@ -0,0 +1,12 @@
+this.hej = "heeeeeeeeeeej";
+var heej = "heej"
+
+function setHej(){
+    document.getElementById("hej").innerHTML = this.hej;
+    var noget = "rip";
+}
+
+function nogetHej(){
+    document.getElementById("hej").innerHTML = "lol";
+}
+
